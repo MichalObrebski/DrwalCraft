e7279c301c65a02012c4e03262c0e6c819bbe49d
Mateusz <mateuszkuprewicz8@gmail.com>
2026-02-06 04:25:09 +0100

    Pierwszy szkic struktury serwer-klient Serwer ma już asynchroniczny podział na czytanie, pisanie, gameloopa Klient jeszcze nie.

---
+++ DrwalCraft.Client/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text.Json;
+
+namespace DrwalCraft.Client;
+
+public static class Program
+{
+    private record Message(string From, string Text);
+    const int Port = 5000;
+    public static async Task Main(string[] args)
+    {
+        var ipEndPoint = new IPEndPoint(IPAddress.Loopback, Port);
+        using var client = new TcpClient();
+        await client.ConnectAsync(ipEndPoint);
+        Console.WriteLine("Client connected");
+        
+        var reader = new StreamReader(client.GetStream());
+        var writer = new StreamWriter(client.GetStream()) { AutoFlush = true };
+
+        while (Console.ReadLine() is { } line)
+        {
+            await writer.WriteLineAsync(line);
+            var response = await reader.ReadLineAsync();
+             if (CheckMessage(response) == "JSON")
+             {
+                 Message? msg = JsonSerializer.Deserialize<Message>(response);
+                 if (msg != null)
+                 {
+                     Console.WriteLine(msg.Text + " from:  " + msg.From);
+                     if(msg.From != "Serwer") Console.WriteLine("A ty?");
+                 }
+             }
+             else
+             {
+                 Console.WriteLine(response);
+             }
+             if (response is null) break;
+            //Console.WriteLine(response);
+        }
+    }
+    static string CheckMessage(string msg)
+    {
+        try
+        {
+            var obj = JsonSerializer.Deserialize<object>(msg);
+            return "JSON";
+        }
+        catch (JsonException)
+        {
+            return "String";
+        }
+    }
+}
